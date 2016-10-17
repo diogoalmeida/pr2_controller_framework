10d19fc3424714d3f13a5146424a829f11a98506
Diogo Almeida <diogoa@kth.se>
2016-10-17 19:18:22 +0200

    Implemented actionlib interface. Requires implementing the control loop for each joint before testing.

--- include/pr2_controller/joint_controller.hpp
+++ include/pr2_controller/joint_controller.hpp
@@ -1,17 +1,31 @@
 #include <pr2_controller_interface/controller.h>
 #include <pr2_mechanism_model/joint.h>
 #include <control_toolbox/pid.h>
+#include <actionlib/server/simple_action_server.h>
+#include <pr2_controller/PR2JointCommandAction.h>
+#include <boost/thread/mutex.hpp>
 
 namespace pr2_joint_controller{
 
 class JointController: public pr2_controller_interface::Controller
 {
 private:
-  pr2_mechanism_model::JointState* joint_state_;
   double init_pos_;
-  control_toolbox::Pid pid_controller_;
+
+  pr2_mechanism_model::JointState* joint_state_;
   pr2_mechanism_model::RobotState *robot_;
+
+  control_toolbox::Pid pid_controller_;
+  sensor_msgs::JointState control_references_;
+  boost::mutex reference_mutex_;
   ros::Time time_of_last_cycle_;
+
+  actionlib::SimpleActionServer<pr2_controller::PR2JointCommandAction> *action_server_;
+  boost::shared_ptr<const pr2_controller::PR2JointCommandGoal> goal_;
+  pr2_controller::PR2JointCommandFeedback feedback_;
+  pr2_controller::PR2JointCommandResult result_;
+  std::string action_name_;
+
 
 public:
   virtual bool init(pr2_mechanism_model::RobotState *robot,
@@ -19,5 +33,8 @@ public:
   virtual void starting();
   virtual void update();
   virtual void stopping();
+
+  void goalCB();
+  void preemptCB();
 };
 }
--- src/joint_controller.cpp
+++ src/joint_controller.cpp
@@ -32,7 +32,66 @@ bool JointController::init(pr2_mechanism_model::RobotState *robot, ros::NodeHand
     return false;
   }
 
+  if (!n.getParam("actionlib_server_name", action_name_))
+  {
+    ROS_WARN("Action name not set. Using default 'PR2_pos_control'");
+    action_name_ = "PR2_pos_control";
+  }
+
+  action_server_ = new actionlib::SimpleActionServer<pr2_controller::PR2JointCommandAction>(n, action_name_, false);
+  action_server_->registerGoalCallback(boost::bind(&JointController::goalCB, this));
+  action_server_->registerPreemptCallback(boost::bind(&JointController::preemptCB, this));
+
   return true;
+}
+
+/// Gets a goal request from actionlib
+void JointController::goalCB()
+{
+  int j = 0;
+  pr2_mechanism_model::JointState *joint_state;
+  goal_ = action_server_->acceptNewGoal();
+
+  boost::lock_guard<boost::mutex> guard(reference_mutex_);
+  control_references_.name.clear();
+  control_references_.position.clear();
+  control_references_.velocity.clear();
+  control_references_.effort.clear();
+
+  for(std::vector<std::string>::const_iterator i = goal_->commanded_joint_state.name.begin(); i != goal_->commanded_joint_state.name.end(); i++)
+  {
+    if (goal_->use_current_position)
+    {
+      joint_state = robot_->getJointState(*i);
+
+      if(!joint_state)
+      {
+        ROS_ERROR("Could not find joint %s", (*i).c_str());
+        action_server_->setAborted();
+        return;
+      }
+      else
+      {
+        control_references_.position.push_back(joint_state->position_);
+      }
+    }
+    else
+    {
+      control_references_.position.push_back(goal_->commanded_joint_state.position[j]);
+    }
+
+    control_references_.velocity.push_back(goal_->commanded_joint_state.velocity[j]);
+    control_references_.effort.push_back(goal_->commanded_joint_state.effort[j]);
+    control_references_.name.push_back(*i);
+    j++;
+  }
+}
+
+/// Handles preemption requests from the actionlib client
+void JointController::preemptCB()
+{
+    ROS_WARN("PR2 Joint Controller preempted!");
+    action_server_->setPreempted();
 }
 
 /// Controller startup in realtime

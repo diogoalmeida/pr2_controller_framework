1b3f20b74820ff7294f9d1e4f11915ae914365e8
Diogo Almeida <diogoa@kth.se>
2016-10-18 08:28:05 +0200

    Finalized initial actionlib implementation with naive pid controller for joint position

--- include/pr2_controller/joint_controller.hpp
+++ include/pr2_controller/joint_controller.hpp
@@ -10,30 +10,36 @@ namespace pr2_joint_controller{
 class JointController: public pr2_controller_interface::Controller
 {
 private:
-  double init_pos_;
-
-  pr2_mechanism_model::JointState* joint_state_;
+  // PR2 state class
   pr2_mechanism_model::RobotState *robot_;
 
-  control_toolbox::Pid pid_controller_;
+  // Control members
   sensor_msgs::JointState control_references_;
-  boost::mutex reference_mutex_;
-  ros::Time time_of_last_cycle_;
+  std::vector<control_toolbox::Pid*> joint_controllers_;
+  std::vector<std::string> joint_names_; // the controller will only accept a command that includes ALL the joint_names_ it's expecting to receive. It ignores all other names
+  std::vector<ros::Time> time_of_last_cycle_; // Per controller
 
+  // Reference mutex for preventing updating the reference halfway through a control cycle
+  boost::mutex reference_mutex_;
+
+  // Actionlib interface members. Action server implemented with the Goal callback method.
   actionlib::SimpleActionServer<pr2_controller::PR2JointCommandAction> *action_server_;
   boost::shared_ptr<const pr2_controller::PR2JointCommandGoal> goal_;
   pr2_controller::PR2JointCommandFeedback feedback_;
   pr2_controller::PR2JointCommandResult result_;
   std::string action_name_;
 
+  double getReferencePosition(std::string joint_name);
 
 public:
+  // Controller interface methods
   virtual bool init(pr2_mechanism_model::RobotState *robot,
                    ros::NodeHandle &n);
   virtual void starting();
   virtual void update();
   virtual void stopping();
 
+  // Actionlib interface methods
   void goalCB();
   void preemptCB();
 };
--- src/joint_controller.cpp
+++ src/joint_controller.cpp
@@ -4,38 +4,38 @@
 namespace pr2_joint_controller {
 
 
-/// Controller initialization in non-realtime
+/*
+  Controller initialization
+*/
 bool JointController::init(pr2_mechanism_model::RobotState *robot, ros::NodeHandle &n)
 {
-  std::string joint_name;
-  if (!n.getParam("joint_name", joint_name))
-  {
-    ROS_ERROR("No joint given in namespace: '%s')",
-              n.getNamespace().c_str());
-    return false;
-  }
-
-  joint_state_ = robot->getJointState(joint_name);
-  if (!joint_state_)
-  {
-    ROS_ERROR("Joint controller could not find joint named '%s'", joint_name.c_str());
-    return false;
-  }
-
   // copy robot pointer so we can access time
   robot_ = robot;
-
-  // construct pid controller
-  if (!pid_controller_.init(ros::NodeHandle(n, "pid_parameters"))){
-    ROS_ERROR("Joint controller could not construct PID controller for joint '%s'",
-              joint_name.c_str());
-    return false;
-  }
 
   if (!n.getParam("actionlib_server_name", action_name_))
   {
     ROS_WARN("Action name not set. Using default 'PR2_pos_control'");
     action_name_ = "PR2_pos_control";
+  }
+
+  if (!n.getParam("actuated_joint_names", joint_names_))
+  {
+    ROS_ERROR("Joint controller requires a set of joint names (actuated_joint_names)");
+    return false;
+  }
+
+  for(int i = 0; i < joint_names_.size(); i++) // initialize the joint controllers. Expecting one set of PID gains per actuated joint
+  {
+    if(!n.hasParam("controller_gains/" + joint_names_[i])) // I'm trusting the user to actually set the gains on this namespace... otherwise, it will use the dynamic reconfig defaults
+    {
+      ROS_ERROR("Joint controller expects controller gains for joint %s (controller_gains/%s)", joint_names_[i].c_str(), joint_names_[i].c_str());
+      return false;
+    }
+
+    // create a controller instance and give it a unique namespace for setting the controller gains
+    joint_controllers_.push_back(new control_toolbox::Pid());
+    time_of_last_cycle_.push_back(robot_->getTime());
+    joint_controllers_[i]->init(ros::NodeHandle(n, "controller_gains/" + joint_names_[i]));
   }
 
   action_server_ = new actionlib::SimpleActionServer<pr2_controller::PR2JointCommandAction>(n, action_name_, false);
@@ -45,10 +45,14 @@ bool JointController::init(pr2_mechanism_model::RobotState *robot, ros::NodeHand
   return true;
 }
 
-/// Gets a goal request from actionlib
+/*
+  Goal callback. It is expected that a goal will include a reference for each
+  joint given in the parameter "actuated_joint_names", otherwise the request will
+  be ignored and the server will enter an aborted state.
+*/
 void JointController::goalCB()
 {
-  int j = 0;
+  int number_of_matching_joints = 0;
   pr2_mechanism_model::JointState *joint_state;
   goal_ = action_server_->acceptNewGoal();
 
@@ -58,59 +62,140 @@ void JointController::goalCB()
   control_references_.velocity.clear();
   control_references_.effort.clear();
 
-  for(std::vector<std::string>::const_iterator i = goal_->commanded_joint_state.name.begin(); i != goal_->commanded_joint_state.name.end(); i++)
+  for (int i = 0; i < goal_->commanded_joint_state.name.size(); i++)
   {
-    if (goal_->use_current_position)
+    if (i >= joint_names_.size())
     {
-      joint_state = robot_->getJointState(*i);
+      ROS_ERROR("Joint controller goal request does not set references for all the commanded joints");
+      action_server_->setAborted();
+      return;
+    }
 
-      if(!joint_state)
+    if (std::find(joint_names_.begin(), joint_names_.end(), goal_->commanded_joint_state.name[i]) != joint_names_.end())
+    {
+      if (goal_->use_current_position)
       {
-        ROS_ERROR("Could not find joint %s", (*i).c_str());
-        action_server_->setAborted();
-        return;
+        number_of_matching_joints++;
+        joint_state = robot_->getJointState(goal_->commanded_joint_state.name[i]);
+
+        if(!joint_state)
+        {
+          ROS_ERROR("Could not find joint %s", goal_->commanded_joint_state.name[i].c_str());
+          action_server_->setAborted();
+          return;
+        }
+        else
+        {
+          control_references_.position.push_back(joint_state->position_);
+        }
       }
       else
       {
-        control_references_.position.push_back(joint_state->position_);
+        control_references_.position.push_back(goal_->commanded_joint_state.position[i]);
+      }
+
+      control_references_.velocity.push_back(goal_->commanded_joint_state.velocity[i]);
+      control_references_.effort.push_back(goal_->commanded_joint_state.effort[i]);
+      control_references_.name.push_back(goal_->commanded_joint_state.name[i]);
+
+      if (number_of_matching_joints == joint_names_.size())
+      {
+        ROS_INFO("Joint controller received a valid request");
+        break;
       }
     }
-    else
-    {
-      control_references_.position.push_back(goal_->commanded_joint_state.position[j]);
-    }
+  }
 
-    control_references_.velocity.push_back(goal_->commanded_joint_state.velocity[j]);
-    control_references_.effort.push_back(goal_->commanded_joint_state.effort[j]);
-    control_references_.name.push_back(*i);
-    j++;
+  if (number_of_matching_joints < joint_names_.size())
+  {
+    ROS_ERROR("Goal request does not set references to all the commanded joints");
+    action_server_->setAborted();
+    return;
   }
 }
 
 /// Handles preemption requests from the actionlib client
 void JointController::preemptCB()
 {
-    ROS_WARN("PR2 Joint Controller preempted!");
-    action_server_->setPreempted();
+  boost::lock_guard<boost::mutex> guard(reference_mutex_);
+  control_references_.name.clear();
+  control_references_.position.clear();
+  control_references_.velocity.clear();
+  control_references_.effort.clear();
+
+  ROS_WARN("PR2 Joint Controller preempted!");
+  action_server_->setPreempted();
 }
 
 /// Controller startup in realtime
 void JointController::starting()
 {
-  init_pos_ = joint_state_->position_;
-  time_of_last_cycle_ = robot_->getTime();
-  pid_controller_.reset();
+  for(int i = 0; i < joint_controllers_.size(); i++)
+  {
+    joint_controllers_[i]->reset();
+    time_of_last_cycle_[i] = robot_->getTime();
+  }
 }
 
 /// Controller update loop in realtime
 void JointController::update()
 {
-  double desired_pos = init_pos_ + 15 * sin(ros::Time::now().toSec());
-  double current_pos = joint_state_->position_;
+  ros::Duration dt;
+  pr2_mechanism_model::JointState *joint_state;
+  double current_position = 0.0;
+  double desired_position = 0.0;
 
-  ros::Duration dt = robot_->getTime() - time_of_last_cycle_;
-  time_of_last_cycle_ = robot_->getTime();
-  joint_state_->commanded_effort_ = pid_controller_.computeCommand(current_pos-desired_pos, dt);
+  boost::mutex::scoped_lock lock(reference_mutex_, boost::try_to_lock); // If no lock is obtained, this means the controller is updating references and we will set the control to the current position
+  if (lock)
+  {
+    for (int i = 0; i < joint_controllers_.size(); i++)
+    {
+      joint_state = robot_->getJointState(joint_names_[i]);
+      if(!joint_state)
+      {
+        ROS_ERROR("Failed to get joint state in joint controller update loop!! This should NEVER happen");
+        break;
+      }
+
+      current_position = joint_state->position_;
+      desired_position = getReferencePosition(joint_names_[i]);
+      dt = robot_->getTime() - time_of_last_cycle_[i];
+      joint_state->commanded_effort_ = joint_controllers_[i]->computeCommand(current_position - desired_position, dt);
+    }
+  }
+  else
+  {
+    ROS_WARN("Joint controller lock fail (possibly updating references)");
+    for (int i = 0; i < joint_controllers_.size(); i++)
+    {
+      joint_state = robot_->getJointState(joint_names_[i]);
+      if(!joint_state)
+      {
+        ROS_ERROR("Failed to get joint state in joint controller update loop!! This should NEVER happen");
+        break;
+      }
+
+      dt = robot_->getTime() - time_of_last_cycle_[i];
+      joint_state->commanded_effort_ = joint_controllers_[i]->computeCommand(0, dt); // keep the current position
+    }
+  }
+}
+
+/*
+  Get the position value in the control references for the given joint
+*/
+double JointController::getReferencePosition(std::string joint_name)
+{
+  for (int i = 0; i < control_references_.name.size(); i++)
+  {
+    if (control_references_.name[i] == joint_name)
+    {
+      return control_references_.position[i];
+    }
+  }
+
+  ROS_ERROR("No reference available for joint name %s. This should NEVER happen.", joint_name.c_str());
+  return 0.0;
 }
 
 /// Controller stopping in realtime

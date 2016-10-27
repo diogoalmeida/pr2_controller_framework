07f803a13e8e8e261da2cdd6168830b8d874e4a4
Diogo Almeida <diogoa@kth.se>
2016-10-27 11:27:03 +0200

    Initial commit

---
+++ include/manipulation_client/manipulation_client.hpp
@@ -0,0 +1,17 @@
+#ifndef __MANIPULATION_CLIENT__
+#define __MANIPULATION_CLIENT__
+
+#include <actionlib/client/simple_action_client.h>
+#include <manipulation_controller/ManipulationControllerAction.h>
+
+namespace manipulation{
+
+  class ManipulationClient
+  {
+  public:
+    ManipulationClient();
+
+  private:
+  };
+  }
+#endif
---
+++ src/manipulation_client.cpp
@@ -0,0 +1,10 @@
+#include <manipulation_client/manipulation_client.hpp>
+
+namespace manipulation{
+
+}
+
+int main(int argc, char **argv)
+{
+  return 0;
+}

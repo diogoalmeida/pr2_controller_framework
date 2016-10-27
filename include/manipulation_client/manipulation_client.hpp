3a302dd8afbee4849a8cda7a6abaa948552557e4
Diogo Almeida <diogoa@kth.se>
2016-10-27 18:17:04 +0200

    Integrated TF. Compute initial eef pose

--- include/manipulation_client/manipulation_client.hpp
+++ include/manipulation_client/manipulation_client.hpp
@@ -1,6 +1,9 @@
 #ifndef __MANIPULATION_CLIENT__
 #define __MANIPULATION_CLIENT__
 
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <actionlib/client/simple_action_client.h>
 #include <manipulation_controller/ManipulationControllerAction.h>
 
@@ -11,11 +14,17 @@ namespace manipulation{
   public:
     ManipulationClient(std::string action_name) : action_client_(action_name, true)
     {
+      nh_ = ros::NodeHandle("~");
       action_name_ = action_name;
       runExperiment();
     }
 
   private:
+    // ros
+    ros::NodeHandle nh_;
+    tf::TransformListener listener_;
+    bool loadParams();
+
     // actionlib
     actionlib::SimpleActionClient<manipulation_controller::ManipulationControllerAction> action_client_;
     std::string action_name_;
@@ -24,9 +33,14 @@ namespace manipulation{
     // Vision feedback
     double vision_timeout_;
     bool waitForTablePose(ros::Duration max_time);
+    std::string surface_frame_name_;
 
     // Experimental setup
     void runExperiment();
+    std::vector<double> initial_pose_offset_;
+    geometry_msgs::PoseStamped surface_frame_pose_;
+    geometry_msgs::PoseStamped initial_eef_pose_;
+    std::string base_link_name_;
   };
   }
 #endif
--- src/manipulation_client.cpp
+++ src/manipulation_client.cpp
@@ -3,6 +3,30 @@
 namespace manipulation{
 
 /*
+  Get the client parameters and abort in case they are not found
+*/
+bool ManipulationClient::loadParams()
+{
+  if(!nh_.getParam("vision/surface_frame_name", surface_frame_name_))
+  {
+    ROS_ERROR("No surface frame name defined (vision/surface_frame_name_)");
+    return false;
+  }
+
+  if(!nh_.getParam("base_link_name", base_link_name_))
+  {
+    ROS_ERROR("No base link frame name defined (base_link_name)");
+    return false;
+  }
+
+  if(!nh_.getParam("initialization/initial_pose_offset", initial_pose_offset_))
+  {
+    ROS_ERROR("No inital eef offset defined (initialization/initial_pose_offset)");
+    return false;
+  }
+}
+
+/*
   Initializes the robot pose based on tag information. Commands the initial
   approach (another action? here?). Monitors vision information to compute
   ground truth data, and registers feedback.
@@ -25,6 +49,9 @@ void ManipulationClient::runExperiment()
     return;
   }
 
+  // At this point I have knowledge of the arm that I want to move (tool frame)
+  // and what is my desired initial pose for the robotic manipulator
+
   //while(experiment_conditions)
   {
     // Send experiment arm to right initial pose
@@ -45,7 +72,30 @@ void ManipulationClient::runExperiment()
 */
 bool ManipulationClient::waitForTablePose(ros::Duration max_time)
 {
+  if (!listener_.waitForTransform(surface_frame_name_, base_link_name_, ros::Time(0), max_time))
+  {
+    return false;
+  }
 
+  surface_frame_pose_.header.stamp = ros::Time::now();
+  surface_frame_pose_.header.frame_id = surface_frame_name_;
+  surface_frame_pose_.pose.position.x = 0;
+  surface_frame_pose_.pose.position.y = 0;
+  surface_frame_pose_.pose.position.z = 0;
+  surface_frame_pose_.pose.orientation.x = 0;
+  surface_frame_pose_.pose.orientation.y = 0;
+  surface_frame_pose_.pose.orientation.z = 0;
+  surface_frame_pose_.pose.orientation.w = 1;
+
+  initial_eef_pose_ = surface_frame_pose_;
+  initial_eef_pose_.pose.position.x = initial_pose_offset_[0];
+  initial_eef_pose_.pose.position.y = initial_pose_offset_[1];
+  initial_eef_pose_.pose.position.z = initial_pose_offset_[2];
+  initial_eef_pose_.pose.orientation.x = 1;
+  initial_eef_pose_.pose.orientation.w = 0;
+
+  listener_.transformPose(base_link_name_, surface_frame_pose_, surface_frame_pose_);
+  return true;
 }
 
 }

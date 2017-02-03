414f6338892b1b1ac2e022a69e47b026975e589c
Diogo Almeida <diogoa@kth.se>
2017-02-03 13:33:07 +0100

    asegfsgsger

--- pr2_cartesian_controllers/src/manipulation_controller.cpp
+++ pr2_cartesian_controllers/src/manipulation_controller.cpp
@@ -441,7 +441,7 @@ namespace cartesian_controllers {
     real_x1 = (-B + std::sqrt(B*B - 4*A*C))/(2*A);
     real_theta1 = std::atan2(center_y, (real_x1 - center_x)) - M_PI;
     real_x2 = (-B - std::sqrt(B*B - 4*A*C))/(2*A);
-    real_theta2 = std::acos((real_x2 - center_x)/hardcoded_length_);
+    real_theta2 = std::acos((real_x2 - center_x)/hardcoded_length_) - M_PI;
 
     feedback_.x_c_1 = real_x1;
     feedback_.theta_c_1 = real_theta1;

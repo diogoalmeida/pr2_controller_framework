37ef3634deab557ea93565ffef6c162cc2c87c5d
Diogo Almeida <diogoa@kth.se>
2017-06-09 10:35:37 +0200

    Added the adaptive controller that will estimate the mechanism degrees of freedom

---
+++ pr2_algorithms/include/pr2_algorithms/mechanism_identification/adaptive_velocity_controller.hpp
@@ -0,0 +1,59 @@
+#ifndef __ADAPTIVE_CONTROL_ALGORITHM__
+#define __ADAPTIVE_CONTROL_ALGORITHM__
+
+#include <ros/ros.h>
+#include <Eigen/Dense>
+#include <math.h>
+#include <pr2_algorithms/algorithm_base.hpp>
+#include <limits>
+#include <stdexcept>
+
+namespace manipulation_algorithms{
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
+  /**
+    Class that implements the adaptive control algorithm that enables the
+    mechanism identification.
+  **/
+  class AdaptiveController : public AlgorithmBase
+  {
+  public:
+    AdaptiveController();
+    ~AdaptiveController();
+
+    virtual bool getParams(const ros::NodeHandle &n);
+
+    /**
+      Computes the relative twist reference for the mechanism and updates the
+      estimates.
+
+      @param wrench The required wrench for the estimation. This can be at the end-effector, as the force will match the contact point and the angular velocity has the same restrictions as the contact's.
+      @param dt The elapsed time between calls.
+      @return The control twist for the relative velocity between parts.
+    **/
+    Vector6d control(const Vector6d &wrench, double dt);
+
+    /**
+      Initialize the adaptive controller estimates.
+
+      @param t Initial translational DOF estimate.
+      @param r Initial rotational DOF estimate.
+    **/
+    void initEstimates(const Eigen::Vector3d &t, const Eigen::Vector3d &r);
+
+  private:
+    double alpha_force_, beta_force_, alpha_torque_, beta_torque_, f_d_, torque_d_, v_d_amp_, w_d_amp_, time_, v_freq, w_freq, alpha_adapt_t_, alpha_adapt_r_;
+    Eigen::Vector3d t_, r_, int_force_, int_torque_;
+
+    /**
+      Compute the integral term in the wrench feedback component of the adaptive controller.
+
+      @param prev The previous value of the integral term.
+      @param v The projection vector.
+      @param error The force/torque error.
+      @param dt The elapsed time.
+      @return The updated integral value.
+    **/
+    Eigen::Vector3d computeIntegralTerm(const Eigen::Vector3d &prev, const Eigen::Vector3d &v, const Eigen::Vector3d &error, double dt);
+  };
+}
+#endif
---
+++ pr2_algorithms/src/mechanism_identification/adaptive_velocity_controller.cpp
@@ -0,0 +1,71 @@
+#include <pr2_algorithms/mechanism_identification/adaptive_velocity_controller.hpp>
+
+namespace manipulation_algorithms{
+
+  AdaptiveController::AdaptiveController()
+  {
+    int_force_ = Eigen::Vector3d::Zero();
+    int_torque_ = Eigen::Vector3d::Zero();
+    t_ << 1, 0, 0;
+    r_ << 0, 0, 1;
+  }
+
+  AdaptiveController::~AdaptiveController(){}
+
+  Vector6d AdaptiveController::control(const Vector6d &wrench, double dt)
+  {
+    Eigen::Vector3d force_error, torque_error, v_f, w_f;
+    Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
+    Vector6d ref_twist;
+    double v_d, w_d;
+
+    time_ += dt;
+
+    force_error = f_d_*wrench.block<3,1>(0,0).normalized() - wrench.block<3,1>(0,0);
+    torque_error = torque_d_*wrench.block<3,1>(3,0).normalized() - wrench.block<3,1>(3,0);
+
+    int_force_ = computeIntegralTerm(int_force_, t_, force_error, dt);
+    v_f = alpha_force_*force_error + beta_force_*int_force_;
+    v_d = v_d_amp_*sin(2*M_PI*v_freq*time_);
+    ref_twist.block<3,1>(0,0) = v_d*t_ - (I - t_*t_.transpose())*v_f;
+    t_ = t_ + -alpha_adapt_t_*v_d*(I - t_*t_.transpose())*v_f*dt;
+
+    int_torque_ = computeIntegralTerm(int_torque_, r_, torque_error, dt);
+    w_f = alpha_torque_*torque_error + beta_torque_*int_torque_;
+    w_d = w_d_amp_*sin(2*M_PI*w_freq*time_);
+    ref_twist.block<3,1>(3,0) = w_d*r_ - (I - r_*r_.transpose())*w_f;
+    r_ = r_ - alpha_adapt_r_*w_d*(I - r_*r_.transpose())*w_f*dt;
+
+    return ref_twist;
+  }
+
+  Eigen::Vector3d AdaptiveController::computeIntegralTerm(const Eigen::Vector3d &prev, const Eigen::Vector3d &v, const Eigen::Vector3d &error, double dt)
+  {
+    Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
+
+    return prev + (I - v*v.transpose())*error*dt;
+  }
+
+  void AdaptiveController::initEstimates(const Eigen::Vector3d &t, const Eigen::Vector3d &r)
+  {
+    t_ = t;
+    r_ = r;
+  }
+
+  bool AdaptiveController::getParams(const ros::NodeHandle &n)
+  {
+    // if (!n.getParam("/ects_controller/alpha", alpha_))
+    // {
+    //   ROS_ERROR("Missing alpha gain (/ects_controller/alpha)");
+    //   return false;
+    // }
+    //
+    // if (!n.getParam("/ects_controller/beta", beta_))
+    // {
+    //   ROS_ERROR("Missing beta value (/ects_controller/beta)");
+    //   return false;
+    // }
+
+    return true;
+  }
+}
